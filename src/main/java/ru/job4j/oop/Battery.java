d1c3bf2618ecbcfed960d4e0f8ba8e1a441e6b86
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-15 21:32:22 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryOne = new Battery(50);
+        Battery batteryTwo = new Battery(15);
+        batteryOne.exchange(batteryTwo);
+    }
+}
