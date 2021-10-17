7949dc4bc12d9989f0e16c06dff40acbf06c1bcc
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-17 13:19:07 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Град обречённый", 300);
+        Book book2 = new Book("Три товарища", 200);
+        Book book3 = new Book("Дюна", 350);
+        Book book4 = new Book("Clean code", 900);
+        Book[] bookArray = new Book[] {book1, book2, book3, book4};
+        for (int i = 0; i < bookArray.length; i++) {
+            System.out.println("Книга называется: "
+                    + bookArray[i].getName() + ". "
+                    + "Количество страниц в книге: "
+                    + bookArray[i].getPage() + ".");
+        }
+        Book temp = bookArray[0];
+        bookArray[0] = bookArray[3];
+        bookArray[3] = temp;
+        for (int i = 0; i < bookArray.length; i++) {
+            if (bookArray[i].getName().equals("Clean code")) {
+                System.out.println("Книга называется: "
+                        + bookArray[i].getName() + ". "
+                        + "Количество страниц в книге: "
+                        + bookArray[i].getPage() + ".");
+            }
+        }
+    }
+}
