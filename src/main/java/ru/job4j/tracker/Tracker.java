3acdf44c0d6d7e4738f0b9d12f01d49ec6fb9842
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-17 18:34:46 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,23 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsWithoutNull = Arrays.copyOf(items, size);
+        Item[] itemEqualsKey = new Item[itemsWithoutNull.length];
+        int size = 0;
+        for (Item item : itemsWithoutNull) {
+            if (item.getName().equals(key)) {
+                itemEqualsKey[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemEqualsKey, size);
     }
 
     public Item findById(int id) {
