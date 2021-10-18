994afe66f7735c0e68671fe76b516a09c89bb4b2
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-18 17:36:21 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman fhm = new Freshman();
+        StudentOOP studOOP = fhm;
+        Object ostud = studOOP;
+    }
+}
