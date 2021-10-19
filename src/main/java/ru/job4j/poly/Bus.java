e7d76425501ea6730696301d21a0f61a75c328fb
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-19 09:18:32 +0300

    3. Интерфейс Input [#181092 #217627]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void go() {
-
+        System.out.println("Автобус поехал.");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println("Количество пассажиров в автобусе: " + count);
     }
 
     @Override
     public double refuel(int count) {
-        return 0;
+        return count;
     }
 }
