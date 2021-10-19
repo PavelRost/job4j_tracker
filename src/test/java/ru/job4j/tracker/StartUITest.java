c2f4e3e5ce955b5059028d371fc802b2778505b5
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-19 17:12:29 +0300

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
