07c269c3e87707a43dc2752b13d65720ae0fae99
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-20 09:22:46 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"one", "two", "five"};
+        try {
+            indexOf(array, "four");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
