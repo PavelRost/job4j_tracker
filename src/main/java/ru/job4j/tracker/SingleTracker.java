daa80ca446e8c7d7c0b2dcd259012e17893de22e
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-21 15:28:19 +0300

    1. Singleton [#94606 #218765]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,40 +1,40 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
-    private static Tracker tracker = null;
+    private static SingleTracker instance = null;
+    private Tracker tracker = new Tracker();
 
     private SingleTracker() {
-
     }
 
-    public static Tracker getInstance() {
-        if (tracker == null) {
-            tracker = new Tracker();
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
         }
-        return tracker;
+        return instance;
     }
 
     public Item add(Item item) {
-        return SingleTracker.getInstance().add(item);
+        return tracker.add(item);
     }
 
     public Item[] findAll() {
-        return SingleTracker.tracker.findAll();
+        return tracker.findAll();
     }
 
     public Item[] findByName(String key) {
-        return SingleTracker.tracker.findByName(key);
+        return tracker.findByName(key);
     }
 
     public Item findById(int id) {
-        return SingleTracker.tracker.findById(id);
+        return tracker.findById(id);
     }
 
     public boolean replace(int id, Item item) {
-        return SingleTracker.tracker.replace(id, item);
+        return tracker.replace(id, item);
     }
 
     public boolean delete(int id) {
-        return SingleTracker.tracker.delete(id);
+        return tracker.delete(id);
     }
 }
