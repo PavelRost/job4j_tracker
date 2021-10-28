45c939801f346f64a2802e4c5e51fef0b23a56e0
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-10-28 08:52:27 +0300

    2.2. Лямбда блок [#249208 #221352]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class LambdaUsage {
     public static void main(String[] args) {
         Comparator<String> cmpDescSize = (left, right) -> {
-            System.out.println("compare - " + right.length() + " : " + left.length());
-            return right.length() - left.length();
+            System.out.println("compare - " + left.length() + " : " + right.length());
+            return Integer.compare(right.length(), left.length());
         };
     }
 }
