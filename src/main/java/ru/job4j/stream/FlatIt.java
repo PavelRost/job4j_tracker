7d4d13a76768a443f54d2d4e86f7f7886af20dd1
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-11-03 10:06:20 +0300

    1. flatMap для итератора

---
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.swing.text.html.Option;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class FlatIt {
+    public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
+        return iteratorToStream(it)
+                .filter(Iterator::hasNext)
+                .map(Iterator::next)
+                .collect(Collectors.toList());
+
+//        return iteratorToStream(it)
+//                .flatMap(f -> Stream.of(f.next()))
+//                .collect(Collectors.toList());
+//
+//        return iteratorToStream(it)
+//                .map(Iterator::next)
+//                .collect(Collectors.toList());
+    }
+
+    private static <T> Stream<T> iteratorToStream(Iterator<T> it) {
+        return StreamSupport
+                .stream(Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED), false);
+    }
+}
