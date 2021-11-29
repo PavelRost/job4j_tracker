e175968a1bbb47f1ff12eb325e3b7110780f5880
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-11-29 18:01:15 +0300

    3. Liquibase. Интеграционные тесты. [#95881]

---
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -0,0 +1,108 @@
+package ru.job4j.tracker;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class SqlTrackerTest {
+
+    private static Connection connection;
+
+    @BeforeClass
+    public static void initConnection() {
+        try (InputStream in =
+                     SqlTrackerTest.class.getClassLoader().getResourceAsStream("test.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            connection = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @AfterClass
+    public static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    @After
+    public void wipeTable() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("delete from items")) {
+            statement.execute();
+        }
+    }
+
+    @Test
+    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertThat(tracker.findById(item.getId()), is(item));
+    }
+
+    @Test
+    public void whenReplaceItemThenSuccess() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item1 = new Item("item1");
+        tracker.add(item1);
+        Item itemNew = new Item("itemNew");
+        tracker.replace(item1.getId(), itemNew);
+        assertEquals(tracker.findById(item1.getId()), tracker.findByName(itemNew.getName()).get(0));
+    }
+
+    @Test
+    public void whenDeleteItemThenSuccess() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertTrue(tracker.delete(item.getId()));
+    }
+
+    @Test
+    public void whenFindAllThenSuccess() {
+        SqlTracker tracker = new SqlTracker(connection);
+        List<Item> items =
+                new ArrayList<>(List.of(new Item("item1"), new Item("item2"), new Item("item3")));
+        items.forEach(tracker::add);
+        assertEquals(items, tracker.findAll());
+    }
+
+    @Test
+    public void whenFindByNameThenSuccess() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertEquals(item, tracker.findByName(item.getName()).get(0));
+    }
+
+    @Test
+    public void whenFindByIdThenSuccess() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertEquals(item, tracker.findById(item.getId()));
+    }
+
+}
