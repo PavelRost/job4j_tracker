a050827fcf9adef672ff1465a61004722536045a
Rostmp <84409690+Rostmp@users.noreply.github.com>
2021-12-23 19:07:31 +0300

    1.9. Статические и не статические методы [#504806]

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Calculator {
+
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static int minus(int y) {
+        return y - x;
+    }
+
+    public int divide(int y) {
+        return y / x;
+    }
+
+    public int multiply(int y) {
+        return x * y;
+    }
+
+    public int sumAllOperation(int y) {
+        return sum(y) + minus(y) + divide(y) + multiply(y);
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int sum = sum(5);
+        int minus = minus(11);
+        int divide = calculator.divide(10);
+        int multiply = calculator.multiply(1);
+        int sumAll = calculator.sumAllOperation(3);
+    }
+}
